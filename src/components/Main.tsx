526f9103353a7d485e4ee3aa5c777f31af29c96f
CoreX20 <korneliussetiawan13@gmail.com>
2024-06-28 20:59:26 +0700

    update ch8

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import OwlCarousel from 'react-owl-carousel';
+import 'owl.carousel/dist/assets/owl.carousel.css';
+import 'owl.carousel/dist/assets/owl.theme.default.css';
 const Main: React.FC = () => {
     const options = {
         loop: true,
